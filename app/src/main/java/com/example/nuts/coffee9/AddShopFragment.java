1755c6a4cdc4333c3050aeab45b37f0955dde594
nuts <thenuts53@gmail.com>
2018-01-18 01:10:06 +0700

    add coffee shop fragment v1.6 back toolbar

--- app/src/main/java/com/example/nuts/coffee9/AddShopFragment.java
+++ app/src/main/java/com/example/nuts/coffee9/AddShopFragment.java
@@ -1,10 +1,12 @@
 package com.example.nuts.coffee9;
 
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -14,7 +16,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Toolbar;
+
+import android.support.v7.widget.Toolbar;
 
 
 /**
@@ -32,15 +35,35 @@ public class AddShopFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_shop, container, false);
         // Inflate the layout for this fragment
-
+        Toolbar toolbar = view.findViewById(R.id.toolbarAddShop);
+        Activity activity = getActivity();
+        assert ((AppCompatActivity)getActivity()) != null;
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Add Coffee Shop");
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         return view;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                AddShopFragment addShop = new AddShopFragment();
+                FragmentManager manager_addShop = getFragmentManager();
+                assert manager_addShop != null;
+                android.support.v4.app.FragmentTransaction as = manager_addShop.beginTransaction();
+                as.remove(addShop);
+                //as.addToBackStack(null);
+                as.commit();
+                return true;
+        }
+        return false;
     }
     public void onBackPressed() {
         //this is only needed if you have specific things
         //that you want to do when the user presses the back button.
         /* your specific things...*/
     }
-
 
 }
