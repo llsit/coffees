a939ad1894f217fb73cb617418db5d475f4f57e1
nuts <thenuts53@gmail.com>
2018-01-28 00:55:00 +0700

    edit add coffee shop not finish v1

--- app/src/main/java/com/example/nuts/coffee9/AddShopFragment.java
+++ app/src/main/java/com/example/nuts/coffee9/AddShopFragment.java
@@ -18,6 +18,19 @@ import android.view.ViewGroup;
 import android.view.Window;
 
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -25,6 +38,15 @@ import android.support.v7.widget.Toolbar;
  */
 public class AddShopFragment extends Fragment {
 
+    EditText nameshop,Addressshop,detail,location,time;
+    Button btn_add;
+    RadioButton min,mid,max,radioPrice;
+
+    private String coffee_ID,name,addressshop,Detail,locat,authorID ;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth auth;
+    FirebaseUser user;
 
     public AddShopFragment() {
         // Required empty public constructor
@@ -48,14 +70,61 @@ public class AddShopFragment extends Fragment {
                 onBackPressed();
             }
         });
+
+        /*public void onRadioButtonClicked(View view) {
+            // Is the button now checked?
+            boolean checked = ((RadioButton) view).isChecked();
+
+            // Check which radio button was clicked
+            switch(view.getId()) {
+                case R.id.radio_pirates:
+                    if (checked)
+                        // Pirates are the best
+                        break;
+                case R.id.radio_ninjas:
+                    if (checked)
+                        // Ninjas rule
+                        break;
+            }
+        }*/
+        addShop(view);
         return view;
     }
 
-    public void onBackPressed() {
-        //this is only needed if you have specific things
-        //that you want to do when the user presses the back button.
-        /* your specific things...*/
+    private void addShop(View view) {
+        nameshop = view.findViewById(R.id.nameshop);
+        Addressshop = view.findViewById(R.id.Addressshop);
+        detail = view.findViewById(R.id.detail);
+        btn_add = view.findViewById(R.id.btn_add);
+        min = view.findViewById(R.id.min);
+        mid = view.findViewById(R.id.mid);
+        max = view.findViewById(R.id.max);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = nameshop.getText().toString();
+                addressshop = Addressshop.getText().toString();
+                Detail = detail.getText().toString();
+                authorID = user.getUid();
+                coffee_ID = mDatabase.push().getKey();
+
+                addDataShop shopData = new addDataShop(name, addressshop,Detail,authorID);
+                mDatabase.child("coffee_shop").child(coffee_ID).setValue(shopData);
+
+                Toast.makeText(getActivity(), "Add Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
+    public void onBackPressed() {
         getActivity().onBackPressed();
     }
 
---
+++ app/src/main/java/com/example/nuts/coffee9/addDataShop.java
@@ -0,0 +1,25 @@
+package com.example.nuts.coffee9;
+
+/*
+ * Created by nuts on 1/27/2018.
+ */
+
+public class addDataShop {
+    public String nameshop;
+    public String Addressshop;
+    public String detail,authorID;
+    public String min,mid,max;
+
+
+
+    public addDataShop() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public addDataShop(String nameshop, String Addressshop,String detail,String authorID) {
+        this.nameshop = nameshop;
+        this.Addressshop = Addressshop;
+        this.detail = detail;
+        this.authorID = authorID;
+    }
+}
