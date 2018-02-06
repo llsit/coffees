9d333941653d7a6851c580e853144242f8501fa5
nuts <thenuts53@gmail.com>
2018-02-07 00:22:02 +0700

    edit home page v1.8

--- app/src/main/java/com/cmu/nuts/coffee9/main/fragment/ListShopFragment.java
+++ app/src/main/java/com/cmu/nuts/coffee9/main/fragment/ListShopFragment.java
@@ -3,11 +3,12 @@ package com.cmu.nuts.coffee9.main.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.cmu.nuts.coffee9.R;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 
 public class ListShopFragment extends Fragment {
@@ -18,13 +19,15 @@ public class ListShopFragment extends Fragment {
     }
 
     private DatabaseReference mDatabase;
-
+    private RecyclerView mlist;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_shop, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_shop, container, false);
+        mlist = view.findViewById(R.id.shop_list);
+        return view;
     }
 
 }
--- app/src/main/java/com/cmu/nuts/coffee9/main/model/Listshop.java
+++ app/src/main/java/com/cmu/nuts/coffee9/main/model/Listshop.java
@@ -1,16 +1,46 @@
 package com.cmu.nuts.coffee9.main.model;
 
-/**
+/*
  * Created by nuts on 1/31/2018.
  */
 
 public class Listshop {
 
     private String nameshop;
-    public Listshop(){
+    private String detail;
+    private String Addressshop;
+
+    public Listshop(String nameshop, String detail, String Addressshop) {
+        this.nameshop = nameshop;
+        this.detail = detail;
+        this.Addressshop = Addressshop;
 
     }
-    public Listshop(String nameshop){
 
+    public Listshop() {
+    }
+
+    public String getNameshop() {
+        return nameshop;
+    }
+
+    public void setNameshop(String nameshop) {
+        this.nameshop = nameshop;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getAddressshop() {
+        return Addressshop;
+    }
+
+    public void setAddressshop(String addressshop) {
+        Addressshop = addressshop;
     }
 }
