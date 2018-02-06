9c85b63b18d33b96899b823fef669fe164628507
nuts <thenuts53@gmail.com>
2018-02-07 02:12:25 +0700

    edit home page v1.8.1

--- app/src/main/java/com/cmu/nuts/coffee9/main/fragment/ListShopFragment.java
+++ app/src/main/java/com/cmu/nuts/coffee9/main/fragment/ListShopFragment.java
@@ -3,6 +3,7 @@ package com.cmu.nuts.coffee9.main.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 
 import com.cmu.nuts.coffee9.R;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ListShopFragment extends Fragment {
 
@@ -20,6 +22,8 @@ public class ListShopFragment extends Fragment {
 
     private DatabaseReference mDatabase;
     private RecyclerView mlist;
+    FirebaseDatabase database;
+    DatabaseReference myref;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +31,12 @@ public class ListShopFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list_shop, container, false);
         mlist = view.findViewById(R.id.shop_list);
+        mlist.setHasFixedSize(true);
+        mlist.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        database = FirebaseDatabase.getInstance();
+        myref = database.getReference("coffee_shop");
+
         return view;
     }
 
