33e8f1deb12c070135b7edcac9d15864f9b7adb8
Sapthawee Srichomthong <Mypandacm@hotmail.co.th>
2018-02-21 01:53:51 +0700

    Fix icon copy code from nut

--- app/src/main/java/com/cmu/nuts/coffee9/main/main.java
+++ app/src/main/java/com/cmu/nuts/coffee9/main/main.java
@@ -125,7 +125,7 @@ public class main extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_profile, menu);
-        edit = menu.findItem(R.id.editProfile);
+        edit = menu.findItem(R.id.setting);
         edit.setVisible(false);
         edit.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
