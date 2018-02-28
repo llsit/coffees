e4f5dd6fdcc0ae48832e5f9fedd595a73fbd178c
Jaylers Srichomthong <Mypandacm@hotmail.co.th>
2018-02-28 17:24:44 +0700

    Add fire base key

---
+++ app/src/main/java/com/cmu/nuts/coffee9/utillity/sharedstring/FirebaseKey.java
@@ -0,0 +1,11 @@
+package com.cmu.nuts.coffee9.utillity.sharedstring;
+
+/**
+ * Created by tcdm053 on 28/2/2561.
+ */
+
+public class FirebaseKey {
+    public static String img_profile_key = "profile-image";
+    public static String img_shared_key = "shared-image";
+    public static String img_shop_key = "shop-image";
+}
