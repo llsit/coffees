9d1d0a47358d849f16c5dfdc3b1dcff9a9092907
Nut <thenuts53@gmail.com>
2018-03-14 00:18:29 +0700

    edit data shop v1.5 ทำได้นิดหน่อย

--- app/src/main/java/com/cmu/nuts/coffee9/main/DataShopActivity.java
+++ app/src/main/java/com/cmu/nuts/coffee9/main/DataShopActivity.java
@@ -1,35 +1,59 @@
 package com.cmu.nuts.coffee9.main;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.cmu.nuts.coffee9.R;
 
-import butterknife.BindView;
-import butterknife.OnClick;
-
 public class DataShopActivity extends AppCompatActivity {
 
-
-    @BindView(R.id.data_shop_img_back) ImageView btn_back_data_shop;
+    private ImageView back;
+    //@BindView(R.id.data_shop_img_back) ImageView btn_back_data_shop;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_shop);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_data_shop_title);
+
+//        back = findViewById(R.id.btn_back_data_shop);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_data_shop_title);
+//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
 
-    @OnClick(R.id.data_shop_img_back) public void OnBack(){
-        Toast.makeText(DataShopActivity.this, "Failed",
-                Toast.LENGTH_SHORT).show();
-        finish();
-    }
+//    @OnClick(R.id.data_shop_img_back) public void OnBack(){
+//        Toast.makeText(DataShopActivity.this, "Failed",
+//                Toast.LENGTH_SHORT).show();
+//        finish();
+//    }
 
 }
