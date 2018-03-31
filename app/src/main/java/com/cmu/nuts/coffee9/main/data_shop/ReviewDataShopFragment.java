12ba9bfbac94011b7a448a51e051cdf96cb9aa68
Sapthawee Srichomthong <Mypandacm@hotmail.co.th>
2018-03-31 22:39:04 +0700

    Fix cannot get database

--- app/src/main/java/com/cmu/nuts/coffee9/main/data_shop/ReviewDataShopFragment.java
+++ app/src/main/java/com/cmu/nuts/coffee9/main/data_shop/ReviewDataShopFragment.java
@@ -28,6 +28,11 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,256 +51,57 @@ public class ReviewDataShopFragment extends Fragment {
         // Required empty public constructor
     }
 
-    //private ListView mListView;
-    //private ArrayList<String> mMeetings = new ArrayList<>();
-
     private ListView listView;
-    private FirebaseDatabase firebaseDatabase;
     private ArrayList<String> arrayList;
     private ArrayAdapter arrayAdapter;
     private Review review;
-    TextView text;
+    @BindView(R.id.data_shop_message) TextView data_shop_message;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_review_data_shop, container, false);
+        ButterKnife.bind(this, view);
         activity = getActivity();
-//        recyclerView = view.findViewById(R.id.review_recycler_view);
-//        swipeRefreshLayout = view.findViewById(R.id.review_swipe_refresh_layout);
-        shopID = getArguments().getString("shop_ID");
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference(Review.tag);
+        if (getArguments() != null) {
+            shopID = getArguments().getString("shop_ID");
+        } else {
+            Toast.makeText(getContext(), "Something wrong, that all weknow", Toast.LENGTH_SHORT).show();
+            activity.finish();
+        }
 
-        //Query query = databaseReference.child("review").orderByChild("sid").equalTo(shopID);
-        text = view.findViewById(R.id.test);
-        //text.setText(shopID);
-//        getReviewDatabase();
-//
-//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                getReviewDatabase();
-//            }
-//        });
-        listView = view.findViewById(R.id.listview);
-//        review = new Review();
+        listView = view.findViewById(R.id.data_shop_list_view);
+        arrayList = new ArrayList<>();
 
-        arrayList = new ArrayList<String>();
-        Query mQueryRef = databaseReference.child("review").equalTo(shopID,"sid");
-
-        mQueryRef.addValueEventListener(new ValueEventListener() {
+        databaseReference = FirebaseDatabase.getInstance().getReference(Review.tag).child(shopID);
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
-                    Review value = dataSnapshot.getValue(Review.class);
-                    assert value != null;
-                    String detail = value.getRid();
-////                String title = bullet.getTitle();
-                    text.setText(shopID);
-                    arrayList.add(detail);
+                Log.d(TAG, "onDataChange: "+ dataSnapshot.getChildrenCount());
+                for (DataSnapshot reviewSnapshot: dataSnapshot.getChildren()) {
+                    Review review = reviewSnapshot.getValue(Review.class);
+                    if (Objects.requireNonNull(review).getRid() != null){
+                        arrayList.add(review.getRid());
+                    } else {
+                        Toast.makeText(getContext(), "Something wrong,We can't get the review right now." + shopID, Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                if (arrayList.isEmpty()){
+                    data_shop_message.setVisibility(View.VISIBLE);
+                } else {
+                    data_shop_message.setVisibility(View.GONE);
                     arrayAdapter = new ArrayAdapter<>(activity, R.layout.item_review_list, R.id.show, arrayList);
                     listView.setAdapter(arrayAdapter);
-                } else {
-                    Toast.makeText(getContext(), "Something wrong, That is all we known ", Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Toast.makeText(getContext(), "Error: , " + databaseError.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
-
-//        mQueryRef.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-//                Review value = dataSnapshot.getValue(Review.class);
-//                String detail = value.getRid();
-////                String title = bullet.getTitle();
-//                text.setText(shopID);
-////                Review item = dataSnapshot.getValue(Review.class);
-////                arrayList.add(item);
-////                arrayAdapter = new ArrayAdapter<>(activity, R.layout.item_review_list, R.id.show, arrayList);
-////                listView.setAdapter(arrayAdapter);
-//            }
-//
-//            @Override
-//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//        databaseReference.orderByChild("sid").equalTo(shopID).addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                if (dataSnapshot.exists()) {
-//                    for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
-//                        Review item = singleSnapshot.getValue(Review.class);
-//                        arrayList.add(item);
-//                    }
-//                }
-
-//                for (DataSnapshot issue : dataSnapshot.getChildren()) {
-//                    // do something with the individual "issues"
-//                    Review value = issue.getValue(Review.class);
-//
-//                    if (issue != null) {
-//
-//                        String message = String.valueOf(value.getSid().equals(shopID));
-////                        String message = (String) issue.child("rid").getValue();
-//                        //String sender = (String) issue.child("sender").getValue();
-//                        arrayList.add(message);
-//                    }
-//                    arrayList.add("GG fail");
-//                }
-                //arrayList.add("GGGG fail");
-
-
-//                for (DataSnapshot item : dataSnapshot.getChildren()) {
-//                    Review value = item.getValue(Review.class);
-//                    if (value != null) {
-//                        arrayList.add("GG fail");
-//                        if (value.getSid() == shopID) {
-//                            if (value != null) {
-//                                arrayList.add(value.getDetail());
-//                                Log.d(TAG, "onChildAdded:" + value.getRid());
-//                            } else {
-//                                //Log.d(TAG, "onChildAdded:" + value.getRid());
-//                                arrayList.add("test1");
-//                                arrayList.add("test2");
-//                                arrayList.add("test3");
-//                                arrayList.add("test4");
-//                                arrayList.add(shopID);
-//                            }
-//                        } else {
-//                            arrayList.add("fail");
-//                        }
-//                    }
-//                    arrayList.add("Very fail");
-//
-//                }
-
-
-//                arrayAdapter = new ArrayAdapter<>(activity, R.layout.item_review_list, R.id.show, arrayList);
-//
-//                listView.setAdapter(arrayAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-
-
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//                for (DataSnapshot item : dataSnapshot.getChildren()) {
-//
-//                    review = item.getValue(Review.class);
-//                    arrayList.add(review.getDetail() + " Wow ");
-//                }
-//                listView.setAdapter(arrayAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-
-
-//        databaseReference.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-//                Toast.makeText(getContext(), "TEXT", Toast.LENGTH_LONG).show();
-//
-//                for (DataSnapshot item : dataSnapshot.getChildren()) {
-//
-//                    Review value = dataSnapshot.getValue(Review.class);
-//
-//                    arrayList.add(value.getDetail());
-//
-//
-//
-//                    //arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
-//                    //listView.setAdapter(arrayAdapter);
-//                }
-//                arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, arrayList);
-//                listView.setAdapter(arrayAdapter);
-//
-//                arrayAdapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-////                Toast.makeText(getContext(),  "TEXT", Toast.LENGTH_LONG).show();
-//                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, mMeetings);
-//                for (DataSnapshot childSnapShot : dataSnapshot.getChildren()) {
-//
-//                    Review review = childSnapShot.getValue(Review.class);
-//                    mMeetings.add(String.valueOf(review));
-//
-////                    String date = (String) childSnapShot.child("address").getValue();
-////                    String datetime = (String) childSnapShot.child("datetime").getValue();
-////                    String name = (String) childSnapShot.child("name").getValue();
-////
-////                    mMeetings.add(date);
-////                    mMeetings.add(datetime);
-////                    mMeetings.add(name);
-//
-//
-//                }
-//
-//                mListView.setAdapter(arrayAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//
-//        });
-
 
         return view;
     }
