c478a4a83ba92ad5556eb1669d48efec12bde26d
Nut <thenuts53@gmail.com>
2018-04-12 03:01:02 +0700

    new design profile not success

--- app/src/main/java/com/cmu/nuts/coffee9/preferences/fragment/PreferencesFragment.java
+++ app/src/main/java/com/cmu/nuts/coffee9/preferences/fragment/PreferencesFragment.java
@@ -2,6 +2,7 @@ package com.cmu.nuts.coffee9.preferences.fragment;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cmu.nuts.coffee9.R;
+import com.cmu.nuts.coffee9.beforlogin.login;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -36,11 +39,21 @@ public class PreferencesFragment extends Fragment {
         return view;
     }
 
-    @OnClick(R.id.img_pref_back) public void onBack(){
+    @OnClick(R.id.pref_logout)
+    public void logOut() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signOut();
+        Intent intent = new Intent(activity, login.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.img_pref_back)
+    public void onBack() {
         activity.finish();
     }
 
-    @OnClick(R.id.pref_profile) public void onProfile(){
+    @OnClick(R.id.pref_profile)
+    public void onProfile() {
         ProfileWithEditFragment profileWithEditFragment = new ProfileWithEditFragment();
         FragmentManager manager = getFragmentManager();
         assert manager != null;
@@ -49,11 +62,13 @@ public class PreferencesFragment extends Fragment {
         fragmentTransaction.commit();
     }
 
-    @OnClick(R.id.pref_account) public void onAccout(){
+    @OnClick(R.id.pref_account)
+    public void onAccout() {
 
     }
 
-    @OnClick(R.id.pref_language) public void onLanguage(){
+    @OnClick(R.id.pref_language)
+    public void onLanguage() {
         LanguageFragment languageFragment = new LanguageFragment();
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
@@ -61,11 +76,13 @@ public class PreferencesFragment extends Fragment {
         transaction.commit();
     }
 
-    @OnClick(R.id.pref_term) public void onTerm(){
+    @OnClick(R.id.pref_term)
+    public void onTerm() {
 
     }
 
-    @OnClick(R.id.pref_about) public void onAbout(){
+    @OnClick(R.id.pref_about)
+    public void onAbout() {
 
     }
 
